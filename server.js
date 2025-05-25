5c3d517d96e1e96f75594a5964b8f3f072e08e7d
EthanVT97 <gyi798107@gmail.com>
2025-05-25 18:01:45 +0630

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+app.use(express.json());
+
+const tokenMap = require("./token-map.json");
+
+app.post("/viber/send_message", async (req, res) => {
+  const fakeToken = req.headers["x-fake-token"];
+  const realToken = tokenMap[fakeToken];
+
+  if (!realToken) return res.status(403).json({ error: "Invalid token" });
+
+  try {
+    const viberRes = await axios.post(
+      "https://chatapi.viber.com/pa/send_message",
+      req.body,
+      {
+        headers: {
+          "X-Viber-Auth-Token": realToken,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.status(viberRes.status).json(viberRes.data);
+  } catch (err) {
+    res.status(500).json({ error: "Viber API Error", detail: err.message });
+  }
+});
+
+app.listen(3000, () => console.log("Gateway running on port 3000"));
